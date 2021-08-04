8aba2e9f45de540127684190db37f0180c06fc0b
KenoKrum <87253869+KenoKrum@users.noreply.github.com>
2021-08-04 19:55:02 +0900

    Update ResizeImagesMan.py

--- ResizeImagesMan.py
+++ ResizeImagesMan.py
@@ -4,24 +4,21 @@ from PIL import Image
 
 f = r'D:\UNIVERSITY CONFERENCE\INTERNET B ROLL\Testing'
 
+
 for file in os.listdir(f):
     f_img = f+"/"+file
     img = PIL.Image.open(f_img)
     width, height = img.size
 
-    if width > height:
-        f_img = f+"/"+file
+    if width >= height:
         basewidth = 1920
-        img = Image.open(f_img)
         wpercent = (basewidth / float(img.size[0]))
         hsize = int((float(img.size[1]) * float(wpercent)))
         img = img.resize((basewidth, hsize), Image.ANTIALIAS)
         img.save(f_img)
 
     if height > width:
-        f_img = f+"/"+file
         baseheight = 1080
-        img = Image.open(f_img)
         hpercent = (baseheight / float(img.size[1]))
         wsize = int((float(img.size[0]) * float(hpercent)))
         img = img.resize((wsize, baseheight), Image.ANTIALIAS)
