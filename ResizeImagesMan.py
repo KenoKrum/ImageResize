88bd7cd1cffcf581e1733d862f5a40c081f42c3d
KenoKrum <87253869+KenoKrum@users.noreply.github.com>
2021-08-04 15:33:09 +0900

    Update ResizeImagesMan.py

--- ResizeImagesMan.py
+++ ResizeImagesMan.py
@@ -1,13 +1,29 @@
 import os
+import PIL
 from PIL import Image
 
-f = r'D:\UNIVERSITY CONFERENCE\INTERNET B ROLL'
+f = r'D:\UNIVERSITY CONFERENCE\INTERNET B ROLL\Testing'
 
 for file in os.listdir(f):
     f_img = f+"/"+file
-    basewidth = 1920
-    img = Image.open(f_img)
-    wpercent = (basewidth / float(img.size[0]))
-    hsize = int((float(img.size[1]) * float(wpercent)))
-    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-    img.save(f_img)
+    img = PIL.Image.open(f_img)
+    width, height = img.size
+
+    if width > height:
+        f_img = f+"/"+file
+        basewidth = 1920
+        img = Image.open(f_img)
+        wpercent = (basewidth / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+        img.save(f_img)
+
+    if height > width:
+        f_img = f+"/"+file
+        baseheight = 1080
+        img = Image.open(f_img)
+        hpercent = (baseheight / float(img.size[1]))
+        wsize = int((float(img.size[0]) * float(hpercent)))
+        img = img.resize((wsize, baseheight), Image.ANTIALIAS)
+        img.save(f_img)
+
